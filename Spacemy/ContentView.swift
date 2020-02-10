1f60464700fabd099a9d1f9fb0532a26070557e0
Giovanni Prisco <gprisco01@gmail.com>
2020-02-10 15:28:49 +0100

    Initial Commit

---
+++ Spacemy/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Spacemy
+//
+//  Created by Giovanni Prisco on 10/02/2020.
+//  Copyright © 2020 Giovanni Prisco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
